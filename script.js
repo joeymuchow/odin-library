f81349dbbc3387cd8502f61595aecbb5158197f2
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-02-10 12:50:11 -0800

    Create displayLibrary function and styling grid
    The displayLibrary function creates elements for each book in the
    myLibrary array to display and appends them to the .book-shelf element.
    I am using css grid for displaying all the books and am using autofill
    to handle how many columns there are.

--- script.js
+++ script.js
@@ -14,3 +14,55 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+function displayLibrary() {
+    const bookShelf = document.querySelector(".book-shelf");
+    // Clear bookShelf
+    bookShelf.replaceChildren();
+
+    for (const book of myLibrary) {
+        const bookContainer = document.createElement("div");
+        bookContainer.setAttribute("class", "book");
+        
+        const bookTitle = document.createElement("h3");
+        bookTitle.textContent = book.title;
+        
+        const bookAuthor = document.createElement("h4");
+        bookAuthor.textContent = book.author;
+        
+        const bookPages = document.createElement("p");
+        bookPages.textContent = book.pages + " pages";
+        
+        const bookRead = document.createElement("p");
+        const yesNo = book.read ? "Yes" : "No";
+        bookRead.textContent = "Read? " + yesNo;
+
+        bookContainer.append(bookTitle, bookAuthor, bookPages, bookRead);
+        bookShelf.append(bookContainer);
+    }
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "300", false);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+addBookToLibrary("Test", "Test Author", "1000", true);
+
+displayLibrary();
