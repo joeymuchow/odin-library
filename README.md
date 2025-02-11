9b20ec3f7968f86380c36e2c868ce8da541e93ff
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-02-10 18:00:10 -0800

    Create button to change read status of a book
    Each book on display now has a button that toggles the status of a book
    having been read. Also updated the readme to describe the project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-library
+
+This a project from The Odin Project Javascript Course where we are creating a library app that allows a user to add books and display them. The books show the title, author, number of pages, status of if the book has been read plus a button to change that status, and a button to remove a book from the library.
--- script.js
+++ script.js
@@ -32,9 +32,14 @@ function submitForm(e) {
 }
 
 function removeBook(e) {
-    console.log(e);
     const id = Number(e.target.dataset.id);
     myLibrary.splice(id, 1);
+    displayLibrary();
+}
+
+function updateReadStatus(e) {
+    const id = Number(e.target.dataset.id);
+    myLibrary[id].changeReadStatus();
     displayLibrary();
 }
 
@@ -46,6 +51,10 @@ function Book(title, author, pages, read) {
     this.info = function() {
         return `${this.title} by ${this.author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
     }
+}
+
+Book.prototype.changeReadStatus = function() {
+    this.read = !this.read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -62,18 +71,31 @@ function displayLibrary() {
         const bookContainer = document.createElement("div");
         bookContainer.setAttribute("class", "book");
         
-        const bookTitle = document.createElement("h3");
+        const bookTitle = document.createElement("h2");
         bookTitle.textContent = myLibrary[i].title;
         
-        const bookAuthor = document.createElement("h4");
+        const bookAuthor = document.createElement("h3");
         bookAuthor.textContent = myLibrary[i].author;
         
         const bookPages = document.createElement("p");
         bookPages.textContent = myLibrary[i].pages + " pages";
         
+        const bookReadContainer = document.createElement("div");
+        bookReadContainer.setAttribute("class", "book-read-container");
+
         const bookRead = document.createElement("p");
         const yesNo = myLibrary[i].read ? "Yes" : "No";
         bookRead.textContent = "Read? " + yesNo;
+
+        const btnContainer = document.createElement("div");
+        const changeReadStatus = document.createElement("button");
+        changeReadStatus.textContent = "Update";
+        changeReadStatus.setAttribute("class", "change-read-status");
+        changeReadStatus.setAttribute("data-id", i);
+        changeReadStatus.addEventListener("click", updateReadStatus);
+        btnContainer.append(changeReadStatus);
+
+        bookReadContainer.append(bookRead, btnContainer);
 
         const removeBtn = document.createElement("button");
         removeBtn.textContent = "Remove book";
@@ -81,7 +103,7 @@ function displayLibrary() {
         removeBtn.setAttribute("data-id", i);
         removeBtn.addEventListener("click", removeBook);
 
-        bookContainer.append(bookTitle, bookAuthor, bookPages, bookRead, removeBtn);
+        bookContainer.append(bookTitle, bookAuthor, bookPages, bookReadContainer, removeBtn);
         bookShelf.append(bookContainer);
     }
 }
